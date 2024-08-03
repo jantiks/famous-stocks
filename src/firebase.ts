4f45a448386da522d9579699c40627a164308a82
jantiks <tigran.arsenyan.2018@gmail.com>
2024-08-03 11:36:50 +0400

    fixed hardcoded api keys

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,14 +9,22 @@ import { getFunctions, httpsCallable } from "firebase/functions";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
+const authDomain = process.env.REACT_APP_AUTH_DOMAIN;
+const projectId = process.env.REACT_APP_PROJECT_ID;
+const storageBucket = process.env.REACT_APP_STORAGE_BUCKET;
+const messagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID;
+const appId = process.env.REACT_APP_APP_ID;
+const measurementId = process.env.REACT_APP_MEASUREMENT_ID;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDokCe_dPGf-evAptOj1FDijNI3z3owOL0",
-  authDomain: "famous-stocks.firebaseapp.com",
-  projectId: "famous-stocks",
-  storageBucket: "famous-stocks.appspot.com",
-  messagingSenderId: "106183287113",
-  appId: "1:106183287113:web:07b8713b994d3be8cc3a98",
-  measurementId: "G-M2H8K546D6"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId
 };
 
 // Initialize Firebase
